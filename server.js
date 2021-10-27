3fe11557f4b99443702e4beda7cd096fb6d8e975
alecvipa <hebrit_626@hotmail.com>
2021-10-27 17:42:39 -0500

    added starter packages

---
+++ server.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+
+// Dependencies
+var express = require("express");
+var exphbs = require("express-handlebars");
+const nodemailer = require("nodemailer");
+const email = process.env.email;
+const superSecretPwd = process.env.superSecretPwd;
+const tokenFb = process.env.tokenAccesoFB
+
+// Create an instance of the express app.
+var app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static("public"));
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 3000;
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Redirect to https://xyncs.com
+// const targetBaseUrl = 'https://www.baaliacademy.com.mx/inicio';
+
+
+
+// Routes
+app.get('/', function (req, res) {
+    // res.redirect(targetBaseUrl);
+    res.render('inicio')
+
+});
+app.get('/inicio', function (req, res) {
+    res.render('inicio')
+});
+app.get('/privacidad', function (req, res) {
+    res.render('privacidad')
+});
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
