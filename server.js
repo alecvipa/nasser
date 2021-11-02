1fb86fc2efd581ef919dd4c0d22cdb6fbeb8ae15
alecvipa <hebrit_626@hotmail.com>
2021-11-02 10:42:35 -0600

    missing fb pixel & google

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.post("/ajax/email", function (request, response) {
     var htmlBody = `<h2>Correo de contacto</h2><p>Nombre: ${request.body.name} </p> <p> Correo electrónico: <a href='mailto: ${request.body.email}'>${request.body.email}</a></p><p>Código Postal: ${request.body.zip}</p><p>Número de contacto:${request.body.number} </p><p>Checkbox: ${request.body.checkbox}</p>`;
     var mail = {
         from: 'Team: Xyncs Web Studio',
-        to: 'hebrit_626@hotmail.com',
+        to: 'contacto@nassermuebles.com',
         subject: '¡Alguien ha dejado sus datos en tu sitio web!',
         html: htmlBody
     };
